4226368a257cca31c85f011d5c7929b6e84b3277
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-31 17:47:48 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/*-----------------------------------------------------------------
+Challenge: 04-addList
+Difficulty: Basic
+Prompt:
+- Write a function called addList that accepts any quantity of numbers as arguments, adds them together and returns the resulting sum.
+- Assume all parameters will be numbers.
+- If called with no arguments, return 0 (zero).
+Examples:
+add(1) //=> 1
+add(1, 50, 1.23) //=> 52.23
+add(7, -12) //=> -5
+
+Hint:  Check out the Further Study section of the JS Functions lesson regarding "rest parameters"
+-----------------------------------------------------------------*/
+// Your solution for 04-addList here:
+
+function addList(...theArgs) {
+  return theArgs.reduce((current, previous) => current + previous, 0);
+}
